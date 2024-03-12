50691ba736ad5e79314ac57eab4320624c893a1e
Mateusz Łatka <115552649+Mateusz-Latka@users.noreply.github.com>
2024-03-12 21:57:08 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  return {
+    name: 'skillsMember',
+    restrict: 'E',
+    templateUrl: 'app/components/member/skills/skills.html',
+    controller: 'SkillsMemberCtrl',
+    controllerAs: 'skillsMember'
+  };
+}
